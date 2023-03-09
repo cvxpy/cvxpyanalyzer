a9d1ea87432a72cc4970b75e86dd084f63e4aa91
BarGoldman <93201414+BarGoldman@users.noreply.github.com>
2023-03-09 13:36:43 +0200

    Update visual.py

--- Visual/visual.py
+++ Visual/visual.py
@@ -3,6 +3,7 @@ from tkinter.ttk import Treeview
 import tkinter as tk
 from tkinter import font, ttk
 
+from future.moves.tkinter import ttk
 import cvxpy as cp
 import cvxopt
 from cvxpy import Minimize, Problem, Variable, quad_form
@@ -32,112 +33,6 @@ def is_float(s):
 
 
 class Visual:
-    """
-    >>> from Visual import visual
-    >>> import numpy as np
-    >>> from Visual import binary_tree
-    >>> from tkinter.ttk import Treeview
-    >>> import tkinter as tk
-    >>> from tkinter import font, ttk
-
-    >>> import cvxpy as cp
-    >>> import cvxopt
-    >>> from cvxpy import Minimize, Problem, Variable, quad_form
-    >>> from cvxpy.problems.objective import Objective
-    >>> import operator
-
-    >>> from Visual.binary_tree import Node
-    >>> n = 3
-    >>> P = cvxopt.matrix([13, 12, -2,12, 17, 6,-2, 6, 12], (n, n))
-    >>> q = cvxopt.matrix([-22, -14.5, 13], (n, 1))
-    >>> r = 1
-    >>> x_star = cvxopt.matrix([1, 1 / 2, -1], (n, 1))
-    >>> x = Variable(n)
-    >>> y = Variable()
-    >>> z = Variable(n)
-    >>> objective = Minimize(0.5 * quad_form(x, P) - cp.sum_squares(x) + q.T @ x + r + y)
-    >>> v = Visual(objective)
-    >>> stri=str(objective.expr).replace("+ -", " - ")
-    >>> stri==v.root.expr
-    True
-    >>> v.root.CheckinSons('-')
-    True
-    >>> n=v.root.NodeSon('-')
-    >>> n.CheckinSons('0.5 @ QuadForm(var1, [[13. 12. -2.][12. 17.  6.][-2.  6. 12.]])')
-    True
-    >>> A_wall = 100
-    >>> A_flr = 10
-    >>> alpha = 0.5
-    >>> beta = 2
-    >>> gamma = 0.5
-    >>> delta = 2
-
-    >>> h = cp.Variable(pos=True, name="h")
-    >>> w = cp.Variable(pos=True, name="w")
-    >>> d = cp.Variable(pos=True, name="d")
-
-    >>> volume = h * w * d
-    >>> wall_area = 2 * (h * w + h * d)
-    >>> flr_area = w * d
-    >>> hw_ratio = h/w
-    >>> dw_ratio = d/w
-    >>> constraints = [wall_area <= A_wall,flr_area <= A_flr,hw_ratio >= alpha,hw_ratio <= beta,dw_ratio >= gamma,dw_ratio <= delta]
-    >>> objective=cp.Maximize(volume)
-    >>> v = Visual(objective)
-    >>> stri=str(objective.expr).replace("+ -", " - ")
-    >>> print(stri)
-    True
-    >>> stri==v.root.expr
-    True
-    >>> v.root.CheckinSons('@')
-    True
-    >>> v.root.CheckinSons('h')
-    False
-    >>> n = v.root.NodeSon('@')
-    >>> n.CheckinSons('h')
-    True
-    >>> n.CheckinSons(n.sons[0].expr)
-    True
-    >>> n.CheckinSons('c')
-    False
-    >>> n.CheckinSons(n.sons[1].expr)
-    True
-    >>> n=n.NodeSon(n.sons[1].expr)
-    >>> n.CheckinSons('@')
-    True
-    >>> n=n.NodeSon('@')
-    >>> n.CheckinSons(n.sons[0].expr)
-    True
-    >>> n.CheckinSons(n.sons[1].expr)
-    True
-    >>> n.CheckinSons('x')
-    False
-
-    >>> objective=cp.Maximize(h)
-    >>> v = Visual(objective)
-    >>> v.root.CheckinSons('+')
-    False
-    >>>  if v.root.sons.len() == 0
-    True
-    >>> objective=Minimize(quad_form(x, P))
-    >>> stri=str(objective.expr).replace("+ -", " - ")
-    >>> print(stri)
-    True
-    >>> v = Visual(objective)
-    >>> v.root.CheckinSons('QuadForm')
-    True
-    >>> v.root.CheckinSons('var1')
-    False
-    >>> n=v.root.NodeSon('QuadForm')
-    >>> n.CheckinSons('var1')
-    True
-    >>> n.CheckinSons(str(P))
-    True
-
-
-
-
-    """
     def __init__(self, obj: Objective):
         self.ob = obj
         self.name = obj.NAME
@@ -149,8 +44,8 @@ class Visual:
         self.root = Node(None, self.expr, 0, 0)
         self.create_lists(self.expr)
         self.split_expr(self.root)
-        # self.root.print_tree()
-        # self.show()
+        self.root.print_tree()
+        self.show()
 
     def create_lists(self, exp):
         isFunc = False
@@ -267,7 +162,6 @@ class Visual:
                 self.create_tree_r(tree, son)
 
     def priority(self, exp):
-
         pr = 0
         ans = ''
         ops = self.create_lists(exp)
@@ -309,47 +203,42 @@ class Visual:
     def draw_constrain(self):
         pass
 
-    def draw_constrain(self):
-        pass
 
-if __name__ == '__main__':
-    import doctest
-    doctest.testmod()
-# n = 3
-# P = cvxopt.matrix([13, 12, -2,
-#                    12, 17, 6,
-#                    -2, 6, 12], (n, n))
-# q = cvxopt.matrix([-22, -14.5, 13], (n, 1))
-# r = 1
-# x_star = cvxopt.matrix([1, 1 / 2, -1], (n, 1))
+n = 3
+P = cvxopt.matrix([13, 12, -2,
+                   12, 17, 6,
+                   -2, 6, 12], (n, n))
+q = cvxopt.matrix([-22, -14.5, 13], (n, 1))
+r = 1
+x_star = cvxopt.matrix([1, 1 / 2, -1], (n, 1))
+
+# Frame and solve the problem
+
+x = Variable(n)
+y = Variable()
+z = Variable(n)
+objective = Minimize(0.5 * quad_form(x, P) - cp.sum_squares(x) + q.T @ x + r + y)
+# objective = Minimize((x - y) ** 2 + quad_form(z, P))
+
+# objective = cp.Minimize((x - y) ** 2)
+constraints = [x >= -1, x <= 1]
+
+# p = Problem(objective, constraints)
+# # The optimal objective is returned by p.solve().
+# result = p.solve()
+print(str(objective.expr))
+
+# expression = "2 * math.pow(3, 2) + math.sqrt(16)"
+# tokens = re.findall('[\d\.]+|\(|\)|\+|\-|\@|\/|\^|\w+\(', str(objective.expr))
 #
-# # Frame and solve the problem
-#
-# x = Variable(n)
-# y = Variable()
-# z = Variable(n)
-# objective = Minimize(0.5 * quad_form(x, P) - cp.sum_squares(x) + q.T @ x + r + y)
-# # objective = Minimize((x - y) ** 2 + quad_form(z, P))
-#
-# # objective = cp.Minimize((x - y) ** 2)
-# constraints = [x >= -1, x <= 1]
-#
-# # p = Problem(objective, constraints)
-# # # The optimal objective is returned by p.solve().
-# # result = p.solve()
-# print(str(objective.expr))
-#
-# # expression = "2 * math.pow(3, 2) + math.sqrt(16)"
-# # tokens = re.findall('[\d\.]+|\(|\)|\+|\-|\@|\/|\^|\w+\(', str(objective.expr))
-# #
-# # print(tokens)
-#
-# # for s in str(objective.expr).split():
-# #     print("s= ", s)
-#
-# v = Visual(objective)
-#
-# print(v.check_2_dimensions())
-# # print(v.func)
-# # print(v.parameters)
-# # print(v.variables)
+# print(tokens)
+
+# for s in str(objective.expr).split():
+#     print("s= ", s)
+
+v = Visual(objective)
+
+print(v.check_2_dimensions())
+# print(v.func)
+# print(v.parameters)
+# print(v.variables)
