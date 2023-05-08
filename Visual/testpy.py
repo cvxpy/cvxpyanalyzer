94ecb789d55d3f676ada40e9048b22001eb9faa2
nivmoti <nivnivnivniv00@gmail.com>
2023-05-08 22:33:11 +0300

    new tests

--- Visual/testpy.py
+++ Visual/testpy.py
@@ -94,7 +94,67 @@ def test_priority():
 
 
 def test_create_lists():
-    pass
+    n = 3
+    P = cvxopt.matrix([13, 12, -2,
+                       12, 17, 6,
+                       -2, 6, 12], (n, n))
+    q = cvxopt.matrix([-22, -14.5, 13], (n, 1))
+    r = 1
+    x_star = cvxopt.matrix([1, 1 / 2, -1], (n, 1))
+
+    x1 = Variable(n)
+    y1 = Variable()
+    x2 = Variable()
+    y2 = Variable()
+    z1 = Variable(n)
+
+    objective = Minimize(-1 * y2 ** 2 + 2 * y2)
+    objective1 = Minimize((x2 - y2) ** 2)
+    objective2 = Minimize(0.5 * quad_form(x1, P) - cp.sum_squares(x1) + q.T @ x1 + r + y1)
+    v = Visual(objective)
+    rightOperators = ['@','+']
+    rightVaribale = ['var4']
+    rightFunc = ['power(var4, 2.0)']
+    v.create_lists(v.expr)
+    for e in rightFunc:
+        assert e in v.func
+    assert not 'QuardForm' in v.func
+    for o in rightOperators:
+        assert o in v.operators
+    assert not '/' in v.operators
+    for s in rightVaribale:
+        assert s in v.variables
+    assert not str(x1.expr) in v.variables
+    v = Visual(objective1)
+    v.create_lists(v.expr)
+    rightOperators = ['-']
+    rightVaribale = ['var3','var4']
+    rightFunc = ['power(var3 - var4, 2.0)']
+    for e in rightFunc:
+        assert e in v.func
+
+    assert not 'QuardForm' in v.func
+    for o in rightOperators:
+        assert o in v.operators
+    assert not '/' in v.operators
+    for s in rightVaribale:
+        assert s in v.variables
+    assert not str(x1.expr) in v.variables
+    v = Visual(objective2)
+    v.create_lists(v.expr)
+    rightOperators = ['-', '+', '@']
+    rightVaribale = ['var1']
+    rightFunc = ['QuadForm(var1, [[13. 12. -2.] [12. 17. 6.] [-2. 6. 12.]])', 'quad_over_lin(var1, 1.0)']
+    for e in rightFunc:
+        assert e in v.func
+    assert not 'power' in v.func
+    for o in rightOperators:
+        assert o in v.operators
+    assert not '/' in v.operators
+    for s in rightVaribale:
+        assert s in v.variables
+    assert not str(x2.expr) in v.variables
+
 
 
 def test_curvature_sign():
